382c012c4e3584f9513a858e41180d9d82d14482
swynnejr <saucersam@gmail.com>
2021-07-06 09:42:28 -0500

    Added solution

--- w3d2_reverse_string.js
+++ w3d2_reverse_string.js
@@ -1,22 +1,30 @@
-// reverseString(input) - your input is a string, and the output
-// of the function should be that string, but backwards
-// this will be a new string, as strings are immutable
-// don't use any built-in functions for reversing a string/array
-// in fact, turning the string into an array is unnecessary
-// "hello" as your input should return "olleh"
-// "Hello!" -> "!olleH"
-// "I like to pet cats..." -> "...stac tep ot ekil I"
-// "a" -> "a"
-// "" -> ""
+
+//Reverse string with no Built in functions
+// function reverseString(string) {
+//     var newString = "";
+//     for (var i = string.length - 1; i >= 0; i--) {
+//         newString += string[i];
+//     }
+//     return newString;
+// }
+// console.log(reverseString('Hello!'))
 
 
-function reverseString(string) {
-    var newString = "";
-    for (var i = string.length - 1; i >= 0; i--) {
-        newString += string[i];
-    }
-    return newString;
+
+
+//Reverse string using built in functions by splitting to an array, then turning it back into a string
+
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
 }
-
-
-console.log(reverseString('Hello!'))
+console.log(reverseString("Hello!"));
