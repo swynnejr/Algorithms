ba1822cee0eb8b7e24e557b4ba74dbca324b7453
swynnejr <saucersam@gmail.com>
2021-08-02 10:46:41 -0500

    balance point

---
+++ w7d1_balancepoint.js
@@ -0,0 +1,41 @@
+
+// My progress
+
+function balancePoint(array)
+
+    var sum = 0;
+    var target = 0;
+    for ( var i = 0; i < array.length-1; i++){
+        sum += array[i];
+    }
+
+    var target = sum/2;
+    while (fwd < target);
+
+
+// Teacher solution
+
+function balancePoint(arr){
+    //create counters for left and right totals
+    let left = 0
+    let right = 0
+    //create iterators for left and right
+    let li = 0
+    let ri = arr.length - 1
+    //loop over the array with both iterators until they cross
+    while(li <= ri){
+        //console.log(left, li, right, ri)
+        //add smallest number and adjust iterater for that side
+        if(left + arr[li] < right + arr[ri]){
+            left += arr[li]
+            li++
+        }else{
+            right += arr[ri]
+            ri--
+        }
+    }
+    //return true if they are balanced false if not
+    return left == right
+}
+array= [7,3,6,2,2]
+console.log(balancePoint(array))
